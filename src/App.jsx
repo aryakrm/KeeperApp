bd62f2d0c4479483b9af6cb4b143069246ab5184
Arya <arya@mahsas-mbp.local>
2022-11-15 15:47:04 +0300

    notes added and mapped

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Note from "./Components/Note";
+import notes from "./notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,8 +1,8 @@
-export default function Note() {
+export default function Note({ title, content }) {
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      <h1>{title}</h1>
+      <p>{content}</p>
     </div>
   );
 }
